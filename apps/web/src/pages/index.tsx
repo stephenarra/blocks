190ac24ec5b696b52195dbea1958af531e208005
Stephen Arra <arraworld@gmail.com>
2023-02-20 17:49:25 -0600

    fix getting started button

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { Button } from "@/lib/ui/button";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import { signIn, useSession } from "next-auth/react";
 
 const variants = {
   top: {
@@ -29,6 +30,8 @@ const variants = {
 };
 
 const Home: NextPage = () => {
+  const { data: user } = useSession();
+
   return (
     <Layout>
       <div className="mx-auto max-w-7xl px-4 py-10 sm:py-12 sm:px-6 md:py-16 lg:py-20 lg:px-14 xl:py-24">
@@ -71,9 +74,15 @@ const Home: NextPage = () => {
             animate="animate"
             transition={{ duration: 0.5 }}
           >
-            <Link href="/login">
-              <Button variant="default">Get Started</Button>
-            </Link>
+            {user ? (
+              <Link href="/dashboard">
+                <Button variant="default">Get Started</Button>
+              </Link>
+            ) : (
+              <Button variant="default" onClick={() => signIn("google")}>
+                Get Started
+              </Button>
+            )}
           </motion.div>
         </div>
       </div>
@@ -88,46 +97,6 @@ const Home: NextPage = () => {
           </p>
         </div>
       </div>
-
-      {/* <h1 className="text-3xl font-bold tracking-tight text-gray-100 sm:text-3xl md:text-5xl"> */}
-      {/* <span className="block xl:inline">Discover reactions</span> */}
-      {/* </h1> */}
-      {/* <p className="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
-            Easily find and follow your favorite YouTube reactions.
-          </p> */}
-      {/* <section className="container grid items-center justify-center gap-6 pt-6 pb-8 md:pt-10 md:pb-12 lg:pt-16 lg:pb-24">
-        <div className="mx-auto flex flex-col items-start gap-4 lg:w-[52rem]">
-          <Image
-            src="/image1.png"
-            height={250}
-            width={250}
-            alt="Hero image"
-            priority
-          />
-          <h1 className="text-3xl font-bold leading-[1.1] tracking-tighter sm:text-5xl md:text-6xl">
-            Start creating today
-          </h1>
-          <p className="max-w-[42rem] leading-normal text-slate-700 sm:text-xl sm:leading-8">
-            Build, share, and collaborate on simple voxel models and scenes.
-          </p>
-        </div>
-        <div className="flex gap-4">
-          <Link href="/login">
-            <Button variant="default">Get Started</Button>
-          </Link>
-        </div>
-      </section> */}
-      {/* <hr className="border-slate-200" />
-      <section className="container grid items-center justify-center gap-6 pt-6 pb-8 md:pt-10 md:pb-12 lg:pt-16 lg:pb-24">
-        <div className="mx-auto flex flex-col items-start gap-4 lg:w-[52rem]">
-          <h2 className="text-3xl font-bold leading-[1.1] tracking-tighter sm:text-5xl md:text-6xl">
-            Explore
-          </h2>
-          <p className="max-w-[42rem] leading-normal text-slate-700 sm:text-xl sm:leading-8">
-            Coming soon.
-          </p>
-        </div>
-      </section> */}
     </Layout>
   );
 };
