ae0af18e6e30b53e968d6b0a1631b6514b589b6f
Stephen Arra <arraworld@gmail.com>
2023-02-21 16:29:06 -0600

    use-redis

--- apps/ws-server/src/websockets/persistence.ts
+++ apps/ws-server/src/websockets/persistence.ts
@@ -3,42 +3,66 @@ import WSSharedDoc from "./WSSharedDoc";
 import { fromUint8Array } from "js-base64";
 
 import { prisma } from "database";
+import { RedisPersistence } from "y-redis";
 
-const persistenceDir = process.env.YPERSISTENCE || "./dbDir";
+const isRedis = !!process.env.REDIS_URL;
 
-console.info('Persisting documents to "' + persistenceDir + '"');
-const LeveldbPersistence = require("y-leveldb").LeveldbPersistence;
-const ldb = new LeveldbPersistence(persistenceDir);
+const writeState = async (docName: string, ydoc: WSSharedDoc) => {
+  const documentState = Y.encodeStateAsUpdate(ydoc);
+  const base64Encoded = fromUint8Array(documentState);
 
-export const persistence = {
-  provider: ldb,
-  bindState: async (docName: string, ydoc: WSSharedDoc) => {
-    console.log("BIND", docName);
+  const model = await prisma.model.findUnique({ where: { id: docName } });
 
-    const persistedYdoc = await ldb.getYDoc(docName);
-    const newUpdates = Y.encodeStateAsUpdate(ydoc);
-    ldb.storeUpdate(docName, newUpdates);
-    Y.applyUpdate(ydoc, Y.encodeStateAsUpdate(persistedYdoc));
-
-    ydoc.on("update", (update) => {
-      ldb.storeUpdate(docName, update);
+  if (model.data !== base64Encoded) {
+    const updatedModel = await prisma.model.update({
+      where: { id: docName },
+      data: { data: base64Encoded },
     });
-  },
-  writeState: async (docName: string, ydoc: WSSharedDoc) => {
-    const documentState = Y.encodeStateAsUpdate(ydoc);
-    const base64Encoded = fromUint8Array(documentState);
 
-    const model = await prisma.model.findUnique({ where: { id: docName } });
-
-    if (model.data !== base64Encoded) {
-      const updatedModel = await prisma.model.update({
-        where: { id: docName },
-        data: { data: base64Encoded },
-      });
-
-      console.log("UPDATE::", updatedModel.id);
-    } else {
-      console.log("NO UPDATES");
-    }
-  },
+    console.log("UPDATE::", updatedModel.id);
+  } else {
+    console.log("NO UPDATES");
+  }
 };
+
+let persistence: {
+  provider: any;
+  bindState: (docName: string, ydoc: WSSharedDoc) => Promise<any>;
+  writeState: (docName: string, ydoc: WSSharedDoc) => Promise<void>;
+};
+
+if (isRedis) {
+  const redisOpts = process.env.REDIS_UR as any;
+  const provider = new RedisPersistence({ redisOpts });
+
+  persistence = {
+    provider,
+    bindState: async (docName: string, ydoc: WSSharedDoc) => {
+      provider.closeDoc(docName);
+      return provider.bindState(docName, ydoc);
+    },
+    writeState,
+  };
+} else {
+  const persistenceDir = process.env.YPERSISTENCE || "./dbDir";
+  console.info('Persisting documents to "' + persistenceDir + '"');
+  const LeveldbPersistence = require("y-leveldb").LeveldbPersistence;
+  const provider = new LeveldbPersistence(persistenceDir);
+
+  persistence = {
+    provider,
+    bindState: async (docName: string, ydoc: WSSharedDoc) => {
+      const persistedYdoc = await provider.getYDoc(docName);
+      const newUpdates = Y.encodeStateAsUpdate(ydoc);
+      provider.storeUpdate(docName, newUpdates);
+      Y.applyUpdate(ydoc, Y.encodeStateAsUpdate(persistedYdoc));
+
+      ydoc.on("update", (update) => {
+        provider.storeUpdate(docName, update);
+      });
+    },
+    writeState,
+  };
+}
+
+export { persistence };
